b515ab22416c1b7f6910469ed92e879c0ddbc297
SQLMickey <mickey@mickeystuewe.com>
2016-12-01 16:33:24 -0800

    DATA NOT NEEDED ANY MORE (SORRY TO YELL)

--- Data/dbo.test_Data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO [dbo].[test] ([id], [Cooldatga]) VALUES (1, 1)
-INSERT INTO [dbo].[test] ([id], [Cooldatga]) VALUES (2, 2)
-INSERT INTO [dbo].[test] ([id], [Cooldatga]) VALUES (3, 10)
-INSERT INTO [dbo].[test] ([id], [Cooldatga]) VALUES (4, 5)
