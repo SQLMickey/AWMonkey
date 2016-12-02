10e67aea85ccbecf994b9ff0d11d101b0135fb46
SQLMickey <mickey@mickeystuewe.com>
2016-12-01 16:30:56 -0800

    New lookup data

---
+++ Data/dbo.test_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO [dbo].[test] ([id], [Cooldatga]) VALUES (1, 1)
+INSERT INTO [dbo].[test] ([id], [Cooldatga]) VALUES (2, 2)
+INSERT INTO [dbo].[test] ([id], [Cooldatga]) VALUES (3, 10)
+INSERT INTO [dbo].[test] ([id], [Cooldatga]) VALUES (4, 5)
